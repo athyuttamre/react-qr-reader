3fb34ffce666f3187dd9a04f03e500dc21e46363
Athyuttam Eleti <athyuttamre@gmail.com>
2017-10-08 02:35:07 -0700

    Remove usage of getDeviceId and depend on modern API

--- src/index.js
+++ src/index.js
@@ -124,19 +124,15 @@ module.exports = class Reader extends Component {
   initiate(props = this.props) {
     const { onError, facingMode, chooseDeviceId } = props
 
-    getDeviceId(facingMode, chooseDeviceId)
-      .then(deviceId => {
-        return navigator.mediaDevices.getUserMedia({
-          video: {
-            deviceId,
-            facingMode: facingMode === 'rear' ? 'environment' : 'user',
-            width: { min: 360, ideal: 1280, max: 1920 },
-            height: { min: 240, ideal: 720, max: 1080 },
-          },
-        })
-      })
-      .then(this.handleVideo)
-      .catch(onError)
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: facingMode === 'rear' ? 'environment' : 'user',
+        width: { min: 360, ideal: 1280, max: 1920 },
+        height: { min: 240, ideal: 720, max: 1080 },
+      },
+    })
+    .then(this.handleVideo)
+    .catch(onError)
   }
   handleVideo(stream) {
     const { preview } = this.els
